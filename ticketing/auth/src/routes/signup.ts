50fe4aa12683e78e6e25919819a2d3696a838dd0
Tsai Wei-Che <108356037@nccu.edu.tw>
2021-02-26 02:17:03 +0800

    add JWT hash password in K8s ENV

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -31,7 +31,7 @@ router.post('/api/users/signup', [
     const userJwt = jwt.sign({
         id: user.id,
         email: user.email
-    }, 'asdf')
+    }, process.env.JWT_KEY!)
     req.session = {
         jwt: userJwt
     }
