31fd6a6f068d7c3d34f3da6f17d44663f2c2dd76
Tsai Wei-Che <108356037@nccu.edu.tw>
2021-02-28 22:35:43 +0800

    add pre-hook and getInitialProps

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,26 +1,28 @@
 import { useState } from 'react'
-import axios from 'axios'
+import Router from 'next/router'
+import useRequest from '../../hooks/use-request' 
 
 export default () => {
-
     const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('') 
+    const [password, setPassword] = useState('')
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: () => Router.push('/')
+    })
+
+    // const [successResponse, setResponse] = useState('')
     const onSubmit = async (event) => {
         event.preventDefault()
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            })
-            console.log(response.data)
-        } catch (error) {
-            console.log(error)
-        }
- 
+        await doRequest()
     }
 
     return (
         <form onSubmit={onSubmit}>
-            <h1>Sign up </h1>
+            <h1>Sign up</h1>
             <div className="form-group">
                 <label>Email Address</label>
                 <input 
@@ -38,6 +40,15 @@ export default () => {
                     type="password"
                 />
             </div>
+            {/* {successResponse && (
+                <div className="success success-msg">
+                    <h4>Congratulations!!</h4>
+                    <ul className="my-1">
+                        <li>Remember your info</li>
+                    </ul>
+                </div>
+            )} */}
+            {errors}
         <button className="btn btn-primary">Sign Up</button>
     </form>
 )
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,11 @@
-const index =  () => {
+const LandingPage =  ({ color }) => {
+    console.log(color)
     return <h1>Landing Page</h1>
 }
 
-export default index
+LandingPage.getInitialProps = () => {
+    console.log('In the server side now')
+    return { color: 'red'}
+}
+
+export default LandingPage
