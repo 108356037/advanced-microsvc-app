a3402e8163fcf24d924a4da1b34c10aaee2a3d15
108356037 <108356037@nccu.edu.tw>
2021-03-02 21:01:26 +0800

    need to fix sync problems on skaffold

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const LandingPage = () => {
+    // axios.get("/api/users/currentuser")
     return (<h1>Landing Page</h1>)
 }
 
@@ -8,17 +9,19 @@ LandingPage.getInitialProps = async ({req}) => {
     console.log(req.headers)
     if (typeof window === 'undefined') {
         const { data } = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-                headers: {
-                    Host: 'ticketing.dev'
-                }
+            'http://ingress.ingress.svc.cluster.local/api/users/currentuser', 
+            {
+                headers: req.headers
             }
         )
         return data
 
     } else {
-
+        const { data } = await axios.get("/api/users/currentuser");
+        console.log(data)
+        return data
     }
+
 }
 
 export default LandingPage
