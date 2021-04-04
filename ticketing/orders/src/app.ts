a19c735c0a0584e7884e7a022067cd8be71862f4
蔡維哲 <tsai_wei_che@caiweizhede-MacBook-Pro.local>
2021-04-04 16:23:24 +0800

    basic orders-svc setup

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError, CurrentUser} from '@158fighterss/common'
+
+import { newTicketRouter } from './routes/new-ticket'
+import { showTicketsRouter } from './routes/show-tickets'
+import { updateTicketsRouter } from './routes/update-tickets'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    })
+)
+
+app.use(CurrentUser)
+app.use(newTicketRouter) 
+app.use(showTicketsRouter)
+app.use(updateTicketsRouter)
+
+app.all('*', async () => {
+    throw new NotFoundError() 
+ })
+
+app.use(errorHandler)
+
+export { app }
