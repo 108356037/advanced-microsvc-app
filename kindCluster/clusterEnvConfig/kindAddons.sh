dc76e8a6b49290fd31d9ff57e711726e8ecc89bd
Tsai Wei-Che <108356037@nccu.edu.tw>
2021-04-11 17:35:11 +0800

    fix kind setup scipt

--- kindCluster/clusterEnvConfig/kindAddons.sh
+++ kindCluster/clusterEnvConfig/kindAddons.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/zsh
 # apply ingress to kind cluster
+kind create cluster --config kind-cluster-config.yaml
 kubectl create secret generic jwt-secret --from-literal JWT_KEY=asdf
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
