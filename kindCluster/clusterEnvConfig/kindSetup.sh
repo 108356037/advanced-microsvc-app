92ac6b59cffc954540ec7badab2c894f02782297
Tsai Wei-Che <108356037@nccu.edu.tw>
2021-04-15 10:45:17 +0800

    modify kind cluster setup script

---
+++ kindCluster/clusterEnvConfig/kindSetup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/zsh
+kind delete cluster
+
+echo "kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+- role: worker
+- role: worker
+- role: worker " | kind create cluster --config -
+
+#secret for ticketing-app
+kubectl create secret generic jwt-secret --from-literal JWT_KEY=asdf
+
+# apply ingress to kind cluster
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
